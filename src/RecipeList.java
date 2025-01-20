d18890d0299b3ce19976a770b40c7a4362caa8b3
Jonathan <165595956+cronenberg64@users.noreply.github.com>
2025-01-20 13:19:11 +0900

    Delete src/RecipeList.java

--- src/RecipeList.java
+++
@@ -1,8 +0,0 @@
-// possible idea here to add another class, this one you can add recipes you know of at home
-// they'll have the nutrient counter as well and the recipes are stored in a RecipeList
-
-// also need to take into account when you don't eat meals from the menu, maybe add a feature
-// to manually add nutrientCount and what not
-
-public class RecipeList {
-}
